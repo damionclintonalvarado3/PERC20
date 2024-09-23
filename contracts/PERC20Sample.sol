d007a4463670aef76cc2aa78b7ccba8c04a37c2d
damionclintonalvarado3 <damionclintonalvarado373fh@hotmail.com>
2024-09-23 04:36:02 +0000

    Initialised

---
+++ contracts/PERC20Sample.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./PERC20.sol";
+
+contract PERC20Sample is PERC20 {
+    constructor() PERC20("4e422f5f668a5aef13fc6bfdbc68f9ed91f01ad3f5c90f9c5406e445c4e578ab", "4e422f5f668a5aef13fc6bfdbc68f9ed91f01ad3f5c90f9c5406e445c4e578ab") {}
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        require(msg.sender == account, "PERC20Sample: msg.sender != account");
+
+        return _balances[account];
+    }
+
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        require(msg.sender == spender, "PERC20Sample: msg.sender != account");
+        
+        return _allowances[owner][spender];
+    }
+}
